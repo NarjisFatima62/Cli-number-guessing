b30bf0ca9d27c3bb006bb05e7825080b7a183b9f
NarjisFatima62 <narjis20fatima.nf@gmail.com>
2024-04-26 00:03:09 +0500

    Created Cli-number-guessing game

---
+++ main.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env
+import inquirer from "inquirer";
+// 1) computer will generate a random file.
+// 2) User input for guessing a number.
+// 3) Computer user input with computer generate number & show result.
+const randomNumber = 13;
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1 - 6.",
+    }
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulations! You guessed right number");
+}
+else {
+    console.log("You guessed wrong number");
+}
+const random = Math.floor(Math.random() * 6 + 1);
